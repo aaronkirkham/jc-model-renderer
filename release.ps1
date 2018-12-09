ee48b938b9977193c2fbd2bf08bdcdfb689b844c
aaronkirkham07@gmail.com <aaronkirkham07@gmail.com>
2018-12-09 19:19:06 +0000

    fix release script

--- release.ps1
+++ release.ps1
@@ -2,5 +2,5 @@ $version = Get-Content ".\src\version.h" | Select-String "#define VERSION_" | Fo
 
 New-Item -ItemType Directory -Force -Path ".\out\Release\bin" | Out-Null
 
-Compress-Archive -Path ".\out\Release\jc3-rbm-renderer.exe" -Force -DestinationPath ".\out\Release\bin\jc3-rbm-renderer-$($version -join ".").zip"
+Compress-Archive -Path ".\out\Release\jc-model-renderer.exe" -Force -DestinationPath ".\out\Release\bin\jc-model-renderer-$($version -join ".").zip"
 Get-ChildItem ".\out\Release" -Recurse -File -Include *.pdb | Compress-Archive -Force -DestinationPath ".\out\Release\bin\artifacts.zip"
