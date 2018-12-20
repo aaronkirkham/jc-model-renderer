b7b4de1cd6609f2f6688c5dc8ce9bbb56b51da8a
aaronkirkham07@gmail.com <aaronkirkham07@gmail.com>
2018-12-20 05:36:05 +0000

    final fix for RTPC packing

--- src/game/formats/runtime_container.cpp
+++ src/game/formats/runtime_container.cpp
@@ -170,6 +170,176 @@ void RuntimeContainer::ReadFileCallback(const std::filesystem::path& filename, c
     assert(rtpc);
 }
 
+void WriteNode(std::ofstream& stream, RuntimeContainer* node, std::unordered_map<std::string, uint32_t>& string_offsets,
+               std::vector<std::pair<uint32_t, jc::RuntimeContainer::Node>>& raw_nodes)
+{
+    using namespace jc::RuntimeContainer;
+
+    auto properties = node->GetProperties();
+    auto containers = node->GetContainers();
+
+    const uint32_t prop_offset            = static_cast<uint32_t>(stream.tellp());
+    const uint32_t _child_offset_no_align = (prop_offset + (sizeof(Property) * properties.size()));
+    uint32_t       child_offset           = jc::ALIGN_TO_BOUNDARY(_child_offset_no_align);
+    const uint32_t prop_data_offset       = (child_offset + (sizeof(Node) * containers.size()));
+
+    std::vector<Property> _raw_properties;
+    _raw_properties.reserve(properties.size());
+
+    std::sort(properties.begin(), properties.end(),
+              [](const RuntimeContainerProperty* a, const RuntimeContainerProperty* b) {
+                  return a->GetNameHash() < b->GetNameHash();
+              });
+
+    // write property data
+    stream.seekp(prop_data_offset);
+    for (const auto& prop : properties) {
+        auto       current_pos = static_cast<uint32_t>(stream.tellp());
+        const auto type        = prop->GetType();
+
+        // alignment
+        if (type != RTPC_TYPE_INTEGER && type != RTPC_TYPE_FLOAT && type != RTPC_TYPE_STRING) {
+            uint32_t alignment = 4;
+
+            if (type == RTPC_TYPE_VEC4 || type == RTPC_TYPE_MAT4X4) {
+                alignment = 16;
+            }
+
+            // write padding bytes
+            auto padding = jc::DISTANCE_TO_BOUNDARY(current_pos, alignment);
+            current_pos += padding;
+
+            static uint8_t PADDING_BYTE = 0x50;
+            for (decltype(padding) i = 0; i < padding; ++i) {
+                stream.write((char*)&PADDING_BYTE, 1);
+            }
+        }
+
+        Property _raw_property{prop->GetNameHash(), current_pos, prop->GetType()};
+        switch (type) {
+            case RTPC_TYPE_INTEGER: {
+                auto value                 = std::any_cast<int32_t>(prop->GetValue());
+                _raw_property.m_DataOffset = static_cast<uint32_t>(value);
+                break;
+            }
+
+            case RTPC_TYPE_FLOAT: {
+                auto value                 = std::any_cast<float>(prop->GetValue());
+                _raw_property.m_DataOffset = *(uint32_t*)&value;
+                break;
+            }
+
+            case RTPC_TYPE_STRING: {
+                auto& value = std::any_cast<std::string>(prop->GetValue());
+
+                auto it = string_offsets.find(value);
+                if (it == string_offsets.end()) {
+                    string_offsets[value] = _raw_property.m_DataOffset;
+
+                    // write the null-terminated string value
+                    stream.write(value.c_str(), value.length());
+                    stream << '\0';
+                }
+                // update the current property data offset
+                else {
+                    _raw_property.m_DataOffset = (*it).second;
+                }
+
+                break;
+            }
+
+            case RTPC_TYPE_VEC2:
+            case RTPC_TYPE_VEC3:
+            case RTPC_TYPE_VEC4:
+            case RTPC_TYPE_MAT4X4: {
+                if (type == RTPC_TYPE_VEC2) {
+                    auto& value = std::any_cast<glm::vec2>(prop->GetValue());
+                    stream.write((char*)&value.x, sizeof(value));
+                } else if (type == RTPC_TYPE_VEC3) {
+                    auto& value = std::any_cast<glm::vec3>(prop->GetValue());
+                    stream.write((char*)&value.x, sizeof(value));
+                } else if (type == RTPC_TYPE_VEC4) {
+                    auto& value = std::any_cast<glm::vec4>(prop->GetValue());
+                    stream.write((char*)&value.x, sizeof(value));
+                } else if (type == RTPC_TYPE_MAT4X4) {
+                    auto& value = std::any_cast<glm::mat4x4>(prop->GetValue());
+                    stream.write((char*)&value[0], sizeof(value));
+                }
+
+                break;
+            }
+
+            case RTPC_TYPE_LIST_INTEGERS:
+            case RTPC_TYPE_LIST_FLOATS:
+            case RTPC_TYPE_LIST_BYTES: {
+                if (type == RTPC_TYPE_LIST_INTEGERS) {
+                    auto& value = std::any_cast<std::vector<int32_t>>(prop->GetValue());
+                    auto  count = static_cast<int32_t>(value.size());
+
+                    stream.write((char*)&count, sizeof(count));
+                    stream.write((char*)value.data(), (count * sizeof(int32_t)));
+                } else if (type == RTPC_TYPE_LIST_FLOATS) {
+                    auto& value = std::any_cast<std::vector<float>>(prop->GetValue());
+                    auto  count = static_cast<int32_t>(value.size());
+
+                    stream.write((char*)&count, sizeof(count));
+                    stream.write((char*)value.data(), (count * sizeof(float)));
+                } else if (type == RTPC_TYPE_LIST_BYTES) {
+                    auto& value = std::any_cast<std::vector<uint8_t>>(prop->GetValue());
+                    auto  count = static_cast<int32_t>(value.size());
+
+                    stream.write((char*)&count, sizeof(count));
+                    stream.write((char*)value.data(), (count * sizeof(uint8_t)));
+                }
+
+                break;
+            }
+
+            case RTPC_TYPE_OBJECT_ID: {
+                auto& value = std::any_cast<std::pair<uint32_t, uint32_t>>(prop->GetValue());
+                stream.write((char*)&value.first, (sizeof(uint32_t) * 2));
+                break;
+            }
+
+            case RTPC_TYPE_EVENTS: {
+                auto& value = std::any_cast<std::vector<std::pair<uint32_t, uint32_t>>>(prop->GetValue());
+                auto  count = static_cast<int32_t>(value.size());
+
+                stream.write((char*)&count, sizeof(count));
+                stream.write((char*)value.data(), (count * (sizeof(uint32_t) * 2)));
+                break;
+            }
+        }
+
+        _raw_properties.emplace_back(std::move(_raw_property));
+    }
+
+    auto child_data_offset = static_cast<uint32_t>(stream.tellp());
+
+    // write properties
+    stream.seekp(prop_offset);
+    stream.write((char*)_raw_properties.data(), (sizeof(Property) * _raw_properties.size()));
+
+    std::sort(containers.begin(), containers.end(),
+              [](const RuntimeContainer* a, const RuntimeContainer* b) { return a->GetNameHash() < b->GetNameHash(); });
+
+    // write child containers
+    stream.seekp(jc::ALIGN_TO_BOUNDARY(child_data_offset));
+    for (auto child : containers) {
+        const auto offset = static_cast<uint32_t>(stream.tellp());
+
+        // TODO: offsets are wrong.. maybe?
+
+        Node _node{child->GetNameHash(), offset, static_cast<uint16_t>(child->GetProperties().size()),
+                   static_cast<uint16_t>(child->GetContainers().size())};
+        raw_nodes.emplace_back(std::make_pair(child_offset, std::move(_node)));
+
+        child_offset += sizeof(Node);
+
+        WriteNode(stream, child, string_offsets, raw_nodes);
+    }
+}
+
 bool RuntimeContainer::SaveFileCallback(const std::filesystem::path& filename, const std::filesystem::path& path)
 {
     const auto rc = RuntimeContainer::get(filename.string());
@@ -187,180 +357,17 @@ bool RuntimeContainer::SaveFileCallback(const std::filesystem::path& filename, c
         stream.write((char*)&header, sizeof(header));
 
         //
-        std::queue<std::pair<uint32_t, RuntimeContainer*>> instance_queue;
-        instance_queue.push({sizeof(Header), rc.get()});
-
-        //
         std::vector<std::pair<uint32_t, Node>>    raw_nodes;
         std::unordered_map<std::string, uint32_t> string_offsets;
 
-        stream.seekp(static_cast<uint32_t>(stream.tellp()) + sizeof(Node));
-        while (!instance_queue.empty()) {
-            const auto& [offset, container] = instance_queue.front();
+        const auto current_pos = static_cast<uint32_t>(stream.tellp());
+        stream.seekp(current_pos + sizeof(Node));
 
-            auto properties = container->GetProperties();
-            auto containers = container->GetContainers();
+        Node _node{rc->GetNameHash(), (current_pos + sizeof(Node)), static_cast<uint16_t>(rc->GetProperties().size()),
+                   static_cast<uint16_t>(rc->GetContainers().size())};
+        raw_nodes.emplace_back(std::make_pair(current_pos, std::move(_node)));
 
-            // sort properties
-            std::sort(properties.begin(), properties.end(),
-                      [&](RuntimeContainerProperty* a, RuntimeContainerProperty* b) {
-                          return a->GetNameHash() < b->GetNameHash();
-                      });
-
-            // sort containers
-            std::sort(containers.begin(), containers.end(),
-                      [&](RuntimeContainer* a, RuntimeContainer* b) { return a->GetNameHash() < b->GetNameHash(); });
-
-            const uint32_t prop_offset            = static_cast<uint32_t>(stream.tellp());
-            const uint32_t _child_offset_no_align = (prop_offset + (sizeof(Property) * properties.size()));
-            uint32_t       child_offset           = jc::ALIGN_TO_BOUNDARY(_child_offset_no_align);
-            const uint32_t prop_data_offset       = (child_offset + (sizeof(Node) * containers.size()));
-
-            Node _node{container->GetNameHash(), prop_offset, static_cast<uint16_t>(properties.size()),
-                       static_cast<uint16_t>(containers.size())};
-            raw_nodes.emplace_back(std::make_pair(offset, std::move(_node)));
-
-            std::vector<Property> _raw_properties;
-            _raw_properties.reserve(properties.size());
-
-            // write property data
-            stream.seekp(prop_data_offset);
-            for (const auto& prop : properties) {
-                auto       current_pos = static_cast<uint32_t>(stream.tellp());
-                const auto type        = prop->GetType();
-
-                // alignment
-                if (type != RTPC_TYPE_INTEGER && type != RTPC_TYPE_FLOAT && type != RTPC_TYPE_STRING) {
-                    uint32_t alignment = 4;
-
-                    if (type == RTPC_TYPE_VEC4 || type == RTPC_TYPE_MAT4X4) {
-                        alignment = 16;
-                    }
-
-                    // write padding bytes
-                    auto padding = jc::DISTANCE_TO_BOUNDARY(current_pos, alignment);
-                    current_pos += padding;
-
-                    static uint8_t PADDING_BYTE = 0x50;
-                    for (decltype(padding) i = 0; i < padding; ++i) {
-                        stream.write((char*)&PADDING_BYTE, 1);
-                    }
-                }
-
-                Property _raw_property{prop->GetNameHash(), current_pos, prop->GetType()};
-                switch (type) {
-                    case RTPC_TYPE_INTEGER: {
-                        auto value                 = std::any_cast<int32_t>(prop->GetValue());
-                        _raw_property.m_DataOffset = static_cast<uint32_t>(value);
-                        break;
-                    }
-
-                    case RTPC_TYPE_FLOAT: {
-                        auto value                 = std::any_cast<float>(prop->GetValue());
-                        _raw_property.m_DataOffset = *(uint32_t*)&value;
-                        break;
-                    }
-
-                    case RTPC_TYPE_STRING: {
-                        auto& value = std::any_cast<std::string>(prop->GetValue());
-
-                        auto it = string_offsets.find(value);
-                        if (it == string_offsets.end()) {
-                            string_offsets[value] = _raw_property.m_DataOffset;
-
-                            // write the null-terminated string value
-                            stream.write(value.c_str(), value.length());
-                            stream << '\0';
-                        }
-                        // update the current property data offset
-                        else {
-                            _raw_property.m_DataOffset = (*it).second;
-                        }
-
-                        break;
-                    }
-
-                    case RTPC_TYPE_VEC2:
-                    case RTPC_TYPE_VEC3:
-                    case RTPC_TYPE_VEC4:
-                    case RTPC_TYPE_MAT4X4: {
-                        if (type == RTPC_TYPE_VEC2) {
-                            auto& value = std::any_cast<glm::vec2>(prop->GetValue());
-                            stream.write((char*)&value, sizeof(value));
-                        } else if (type == RTPC_TYPE_VEC3) {
-                            auto& value = std::any_cast<glm::vec3>(prop->GetValue());
-                            stream.write((char*)&value, sizeof(value));
-                        } else if (type == RTPC_TYPE_VEC4) {
-                            auto& value = std::any_cast<glm::vec4>(prop->GetValue());
-                            stream.write((char*)&value, sizeof(value));
-                        } else if (type == RTPC_TYPE_MAT4X4) {
-                            auto& value = std::any_cast<glm::mat4x4>(prop->GetValue());
-                            stream.write((char*)&value, sizeof(value));
-                        }
-
-                        break;
-                    }
-
-                    case RTPC_TYPE_LIST_INTEGERS:
-                    case RTPC_TYPE_LIST_FLOATS:
-                    case RTPC_TYPE_LIST_BYTES: {
-                        if (type == RTPC_TYPE_LIST_INTEGERS) {
-                            auto& value = std::any_cast<std::vector<int32_t>>(prop->GetValue());
-                            auto  count = static_cast<int32_t>(value.size());
-
-                            stream.write((char*)&count, sizeof(count));
-                            stream.write((char*)value.data(), (count * sizeof(int32_t)));
-                        } else if (type == RTPC_TYPE_LIST_FLOATS) {
-                            auto& value = std::any_cast<std::vector<float>>(prop->GetValue());
-                            auto  count = static_cast<int32_t>(value.size());
-
-                            stream.write((char*)&count, sizeof(count));
-                            stream.write((char*)value.data(), (count * sizeof(float)));
-                        } else if (type == RTPC_TYPE_LIST_BYTES) {
-                            auto& value = std::any_cast<std::vector<uint8_t>>(prop->GetValue());
-                            auto  count = static_cast<int32_t>(value.size());
-
-                            stream.write((char*)&count, sizeof(count));
-                            stream.write((char*)value.data(), (count * sizeof(uint8_t)));
-                        }
-
-                        break;
-                    }
-
-                    case RTPC_TYPE_OBJECT_ID: {
-                        auto& value = std::any_cast<std::pair<uint32_t, uint32_t>>(prop->GetValue());
-                        stream.write((char*)&value, (sizeof(uint32_t) * 2));
-                        break;
-                    }
-
-                    case RTPC_TYPE_EVENTS: {
-                        auto& value = std::any_cast<std::vector<std::pair<uint32_t, uint32_t>>>(prop->GetValue());
-
-                        auto count = static_cast<int32_t>(value.size());
-                        stream.write((char*)&count, sizeof(count));
-                        stream.write((char*)value.data(), (count * (sizeof(uint32_t) * 2)));
-                        break;
-                    }
-                }
-
-                _raw_properties.emplace_back(std::move(_raw_property));
-            }
-
-            auto child_data_offset = static_cast<uint32_t>(stream.tellp());
-
-            // write properties
-            stream.seekp(prop_offset);
-            stream.write((char*)_raw_properties.data(), (sizeof(Property) * _raw_properties.size()));
-
-            // write child containers
-            stream.seekp(jc::ALIGN_TO_BOUNDARY(child_data_offset));
-            for (auto child : containers) {
-                instance_queue.push({child_offset, child});
-                child_offset += sizeof(Node);
-            }
-
-            instance_queue.pop();
-        }
+        WriteNode(stream, rc.get(), string_offsets, raw_nodes);
 
         // write the nodes
         for (const auto& node : raw_nodes) {
