6abf37f64d4c2b5b6bfac41718cc7ebc235b8d14
aaronkirkham07@gmail.com <aaronkirkham07@gmail.com>
2019-12-16 17:34:35 +0000

    add RenderBlockCharacter to render block factory

--- src/game/render_block_factory.cpp
+++ src/game/render_block_factory.cpp
@@ -249,7 +249,7 @@ const std::vector<const char*>& RenderBlockFactory::GetValidRenderBlocks()
         "BuildingJC3", "CarLight",     "CarPaintMM", "Character", "CharacterSkin", "General",
         "GeneralJC3",  "GeneralMkIII", "Landmark",   "Prop",      "Window"
 #else
-        "CarPaintMM", "GeneralMkIII"
+        "CarPaintMM", "Character", "GeneralMkIII"
 #endif
     };
     return valid_blocks;
